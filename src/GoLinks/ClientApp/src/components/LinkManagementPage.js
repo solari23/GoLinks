05779a63b30c88f7017a46237f000bad1be701fa
Alexandre Kerametlian <akerametlian@live.com>
2020-04-24 00:46:11 -0700

    Remove the cached serviceworker implementation

--- src/GoLinks/ClientApp/src/components/LinkManagementPage.js
+++ src/GoLinks/ClientApp/src/components/LinkManagementPage.js
@@ -26,7 +26,6 @@ export default class LinkManagementPage extends Component {
                 </ul>
                 <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
                 <p>Env: {process.env.NODE_ENV}</p>
-                <p>Foo: {window.location.origin}</p>
             </div>
         );
     }
--- src/GoLinks/ClientApp/src/index.js
+++ src/GoLinks/ClientApp/src/index.js
@@ -3,12 +3,13 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // ----------------------------------------------------------------------------------------------------------
 
-import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
+
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+import 'bootstrap/dist/css/bootstrap.css';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
@@ -19,5 +20,3 @@ ReactDOM.render(
     </HashRouter>,
     rootElement
 );
-
-registerServiceWorker();
